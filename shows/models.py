b4291004375e511fac3ef960f480ca32af80dd92
Jake Mulhern <mulherje@gmail.com>
2021-01-23 00:38:58 -0600

    Fixing Database

--- shows/models.py
+++ shows/models.py
@@ -12,5 +12,6 @@ class Concert(models.Model):
 class Song(models.Model):
     concert = models.ForeignKey(Concert, on_delete=models.CASCADE)
     song_title = models.CharField(max_length=100)
+    song_location = models.CharField(max_length=200, default='')
     def __str__(self):
         return self.song_title
