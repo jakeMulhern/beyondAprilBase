c1c575616595417e5b71ce8ed5712ba9327951a6
Jake Mulhern <mulherje@gmail.com>
2021-01-22 23:07:19 -0600

    Updated Django Admin Page to include songs

--- shows/admin.py
+++ shows/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import Concert
+from .models import Concert, Song
 
-admin.site.register(Concert)
+
+class SongInLine(admin.StackedInline):
+    model = Song
+
+class ConcertAdmin(admin.ModelAdmin):
+    fieldsets = [
+      ('Date', {'fields': ['concert_date']}), 
+      ('Venue', {'fields': ['venue_name']}),
+      ('City', {'fields': ['city_name'], 'classes':
+      ['collapse']}),
+    ]
+    inlines = [SongInLine]
+
+
+admin.site.register(Concert, ConcertAdmin)
+admin.site.register(Song)
