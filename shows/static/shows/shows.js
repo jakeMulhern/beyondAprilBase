aaa003c9f5375f524efdbfe0f5c28eefeeb98e84
Jake Mulhern <mulherje@gmail.com>
2021-02-01 22:56:44 -0600

    Audio player now pauses, continues, and plays different tracks correctly.

--- shows/static/shows/shows.js
+++ shows/static/shows/shows.js
@@ -5,22 +5,23 @@ var App = {
 		this.bindEvents();
 	},
 	bindEvents: function() {
-		var testItem = document.getElementById('songList');
-		console.log(testItem);
 		document.getElementById('songList').addEventListener('click', function(event) {
 			var elementClicked = event.target;
 			if (elementClicked.className === 'audioTrackName') {
-				this.changeAudioTrack(elementClicked.id);
+				this.playAudioTrack(elementClicked.id);
 				}    
 			}.bind(this));
 	},
-	changeAudioTrack: function(songLocation) {
+	playAudioTrack: function(songLocation) {
 		const audioPlayer = document.getElementById('audioPlayer');
-		audioPlayer.src = songLocation;
-		audioPlayer.play();
-	},
-	makeAlert(elementClicked) {
-		alert(elementClicked.id);
+		if (audioPlayer.paused !== true) {
+			audioPlayer.pause();
+		} else if (songLocation === audioPlayer.src) {
+			audioPlayer.play();
+		} else {
+			audioPlayer.src = songLocation;
+			audioPlayer.play();
+		};
 	}
 }
 
